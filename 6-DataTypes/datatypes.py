53d230e1e2936464afd96338b75a1a8e453fe339
msattdev <mattsattdev@gmail.com>
2026-01-20 16:57:23 -0500

    updates

---
+++ 6-DataTypes/datatypes.py
@@ -0,0 +1,9 @@
+a = 10 # Python infers 'a' as an integer
+b = "Hello" # Python infers 'b' as a string
+c = True # Python infers 'c' as a boolean
+print(a) # Python prints the value of 'a'
+print(b) # Python prints the value of 'b'
+print(c)
+print(type(a)) # Python prints the type of 'a'
+print(type(b)) # Python prints the type of 'b'
+print(type(c)) # Python prints the type of 'c'
