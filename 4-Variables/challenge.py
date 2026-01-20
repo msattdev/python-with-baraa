86df5a13caa9be0951895ee4178f179fa6887da2
msattdev <mattsattdev@gmail.com>
2026-01-20 15:56:31 -0500

    variable challenge

---
+++ 4-Variables/challenge.py
@@ -0,0 +1,10 @@
+# Print the following three lines
+# Add a variable to make it dynamic
+# info@datawithbaraa.com
+# support@datawithbaraa.com
+# www.datawithbaraa.com
+
+name = 'matt'
+print(f'info@datawith{name}.com')
+print(f'support@datawith{name}.com')
+print(f'www.datawith{name}.com')
