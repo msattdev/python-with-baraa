c7d326e51311995a8ca99119cd7a89bf9696cecb
msattdev <mattsattdev@gmail.com>
2026-01-20 15:14:04 -0500

    print updates

--- Print Function/app.py
+++ Print Function/app.py
@@ -7,3 +7,8 @@ print('-------------------------')
 
 
 print("Hi \"PYTHON\"") # Demonstrating using double quotes with escape sequence
+print('Hi "PYTHON"') # Demonstrating using single quotes for the string in order to use double quotes IN the string 
+print("Path: C:\\Users\\Matt\n") # Demonstrating the use of escape sequence for backslash
+
+print("Message1\nMessage2")
+print("Message3\tMessage4")
