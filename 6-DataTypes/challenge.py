21df170894aeea03279e1134af0683844e37e32b
msattdev <mattsattdev@gmail.com>
2026-01-21 10:14:50 -0500

    added challenge file

---
+++ 6-DataTypes/challenge.py
@@ -0,0 +1,21 @@
+age = 35
+height = 5.9
+name = "Matt Satt"
+is_student = False
+courses = "" 
+
+print(age)
+print(type(age))
+# print(len(age)) # This will raise an error since 'age' is an int
+print(height)
+print(type(height))
+# print(len(height)) # This will raise an error since 'height' is a float
+print(name)
+print(type(name))
+print(len(name))
+print(is_student)
+print(type(is_student))
+# print(len(is_student)) # This will raise an error since 'is_student' is a bool
+print(courses)
+print(type(courses))
+print(len(courses))  # This will print 0 since 'courses' is an empty string
