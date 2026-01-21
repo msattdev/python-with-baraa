a07635ef3913e1c539fd38068181c7d5027c53b7
msattdev <mattsattdev@gmail.com>
2026-01-21 10:08:48 -0500

    methods and functions update

---
+++ 6-DataTypes/methodsfunctions.py
@@ -0,0 +1,13 @@
+# text = 'hi'
+# number = 10
+
+# print(type(text))
+# print(type(number))
+# print(len(text))
+# print(len(number)) # error since 'number' is an integer
+
+text = 'hi'
+number = 10
+
+print(text.upper())
+print(number.bit_length())
