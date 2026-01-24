452f975f72a2b59628de969af90d18911f26e4ae
msattdev <mattsattdev@gmail.com>
2026-01-24 14:54:41 -0500

    more fstrings

--- 7-StringFunctions/fstring.py
+++ 7-StringFunctions/fstring.py
@@ -3,3 +3,9 @@ age = 35
 is_student = True
 print('My name is ' + name + ' and I am ' + str(age) + ' years old and student status is ' + str(is_student))
 print(f'My name is {name}. I am {age} years old and my student status is {is_student}.')
+
+# Math with f-strings
+print(f'2 + 3 = {2 + 3}')
+
+# Use curly brackets with f-strings
+print(f'I really want to use {{Curly Brackets}} in my f-string!')
