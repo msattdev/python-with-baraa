fa4d7f4df5e0565f9470b386d089f8f91407f777
msattdev <mattsattdev@gmail.com>
2026-01-23 23:01:54 -0500

    updates

--- 7-StringFunctions/replace.py
+++ 7-StringFunctions/replace.py
@@ -1,6 +1,7 @@
 # Transformations
-price = '1234,56'
-print(price.replace(',','.'))
+price = '$1,299.99'
+print(price.replace('$','').replace(',',''))  # Removing $ and , from the price string
 
 phone = '176-1234-56'
-print(phone.replace('-','/'))
+print(phone.replace('-',' '))
+
