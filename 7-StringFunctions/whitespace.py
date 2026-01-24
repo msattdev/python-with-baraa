306d858884930bb493b17ac51435d06fe8eb682c
msattdev <mattsattdev@gmail.com>
2026-01-24 15:40:25 -0500

    stripping init

---
+++ 7-StringFunctions/whitespace.py
@@ -0,0 +1,12 @@
+# Cleaning
+# Strip whitespace from the left side of the string
+name1 = ' Max'
+print(name1.lstrip())
+
+# Strip whitespace from the right side of the string
+name2 = 'Anna '
+print(name2.rstrip())
+
+# Strip whitespace from both sides of the string
+name3 = '    John Doe   '
+print(name3.strip())
