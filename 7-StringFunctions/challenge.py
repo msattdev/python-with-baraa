57169b4fe0fb67cee086743ea10d1ad6a54c8c4c
msattdev <mattsattdev@gmail.com>
2026-01-23 23:06:20 -0500

    challenge complete

---
+++ 7-StringFunctions/challenge.py
@@ -0,0 +1,5 @@
+# Convert the messy phone number into a clean number format with only digits
+messy_phone = '+49 (176) 123-4567'
+# Needs to be 00491761234567
+messy_phone = messy_phone.replace('+','00').replace(' ','').replace('(','').replace(')','').replace('-','')
+print(messy_phone)
