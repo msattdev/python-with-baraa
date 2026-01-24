ee7d38d7ba0f012ea5f86629e919fec3c654fcaa
msattdev <mattsattdev@gmail.com>
2026-01-23 22:51:40 -0500

    updates

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ age = 35
 print(type(age))  # int
 # print('Your age is: ' + age)  # This will raise an error since 'age' is an int
 print('Your age is: ' + str(age))  # Correct way to concatenate int with str
+
+age = age + 5
+print(age)
+age = str(age)  # Converting int to str
+print(type(age))  # str
+# age = age + 5  # This will now concatenate since 'age' is a str
+print(age)
+
+# Math 
+password = '123a'
+len(password)  # 4
+print(len(password))
