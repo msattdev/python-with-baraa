43f3981e8f621daa38c0a0c78beddc91d6af6c94
msattdev <mattsattdev@gmail.com>
2026-01-24 15:00:45 -0500

    more split practice

--- 7-StringFunctions/split.py
+++ 7-StringFunctions/split.py
@@ -1,3 +1,7 @@
 # Transformations
-stamp = '2026-09-20 14:30'
-print(stamp.split(' '))
+stamp = '2026-09-20'
+print(stamp.split('-'))
+
+# split with comma
+csv_file = '1234,Max,USA,1970-01-01,M'
+print(csv_file.split(','))
