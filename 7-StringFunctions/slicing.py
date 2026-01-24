914a394b33628dfc004fb646e0cdbf9207f07d5d
msattdev <mattsattdev@gmail.com>
2026-01-24 15:34:46 -0500

    slicing init

---
+++ 7-StringFunctions/slicing.py
@@ -0,0 +1,9 @@
+# Index and Slicing
+date = '2024-09-15'
+
+# Extract the year
+print(date[0:4]) # 4 is up to and not including
+print(date[:4]) # You dont have to specify the zero if you are starting from the beginning
+
+# Extract the month
+print(date[5:7])
