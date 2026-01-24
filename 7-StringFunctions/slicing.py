cf49c396ce0e0c9c3f92fec3825a955730c6a0d7
msattdev <mattsattdev@gmail.com>
2026-01-24 15:35:56 -0500

    slicing update

--- 7-StringFunctions/slicing.py
+++ 7-StringFunctions/slicing.py
@@ -7,3 +7,6 @@ print(date[:4]) # You dont have to specify the zero if you are starting from the
 
 # Extract the month
 print(date[5:7])
+
+# Extract the day
+print(date[-2:]) # Use negative numbers up to and including the end
