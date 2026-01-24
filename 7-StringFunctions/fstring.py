e84019a6661e4a6120fc6e2e23fe4629f43efa9f
msattdev <mattsattdev@gmail.com>
2026-01-24 14:49:05 -0500

    fstrings

---
+++ 7-StringFunctions/fstring.py
@@ -0,0 +1,5 @@
+name = 'Matt'
+age = 35
+is_student = True
+print('My name is ' + name + ' and I am ' + str(age) + ' years old and student status is ' + str(is_student))
+print(f'My name is {name}. I am {age} years old and my student status is {is_student}.')
--- 7-StringFunctions/transformations.py
+++ 7-StringFunctions/transformations.py
@@ -1,4 +1,9 @@
-first_name = 'Michael'
-last_name = 'Scott'
-last_name = first_name + ' ' + last_name
-print(last_name)
+# first_name = 'Michael'
+# last_name = 'Scott'
+# last_name = first_name + ' ' + last_name
+# print(last_name)
+
+folder = "C:/Users/Matt/"
+file = 'report.csv'
+path = folder + file
+print(path)
