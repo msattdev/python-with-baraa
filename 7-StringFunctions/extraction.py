7893741f5a36126e3245b89f9d601d9631719a11
msattdev <mattsattdev@gmail.com>
2026-01-24 15:06:03 -0500

    extraction

---
+++ 7-StringFunctions/extraction.py
@@ -0,0 +1,14 @@
+# indexing and slicing
+text = 'Python'
+
+# Extract the first character
+print(text[0])
+print(text[-6])
+
+# Extract last letter
+print(text[-1])
+print(text[5])
+
+# Extract h
+print(text[3])
+print(text[-3])
