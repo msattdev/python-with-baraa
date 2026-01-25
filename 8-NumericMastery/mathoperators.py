7f644e75a75ec7675b24de934d341bef2816e45c
msattdev <mattsattdev@gmail.com>
2026-01-25 18:40:13 -0500

    operators updates

--- 8-NumericMastery/mathoperators.py
+++ 8-NumericMastery/mathoperators.py
@@ -6,3 +6,26 @@ print(7 / 2)
 print(7 // 2) # floor division
 print(7 % 2) # modulo, leftover
 print(2 ** 3) # exponentiation
+
+x = 2
+x += 3
+print(x)
+y = 5
+y -= 2
+print(y)
+z = 3
+z *= 4
+print(z)
+a = 8
+a /= 2
+print(a)
+b = 7
+b //= 3
+print(b)
+c = 10
+c %= 3
+print(c)
+d = 2
+d **= 4
+print(d)
+# --- IGNORE ---
