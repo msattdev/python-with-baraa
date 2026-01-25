f2f4cc6c3a189ce6740018be215d89e49a6b863f
msattdev <mattsattdev@gmail.com>
2026-01-25 18:30:01 -0500

    numbers update

--- 8-NumericMastery/app.py
+++ 8-NumericMastery/app.py
@@ -1,8 +1,22 @@
 # Numbers
-y = 5
-x = 5.7
-z = 2 + 3j
+# y = 5
+# x = 5.7
+# z = 2 + 3j
 
-print(type(y))
+# print(type(y))
+# print(type(x))
+# print(type(z))
+
+
+x = '24'
 print(type(x))
-print(type(z))
+x = int(x)
+print(type(x))
+print(x)
+print(x * 7)
+
+y = 3.14
+print(int(y))
+
+z = 3
+print(float(z))
