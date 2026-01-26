399df6d4789ad653cd4c357718caa2173506eb73
msattdev <mattsattdev@gmail.com>
2026-01-25 19:06:54 -0500

    advanced math updates

---
+++ 8-NumericMastery/advancedmath.py
@@ -0,0 +1,12 @@
+import random
+print(random.random())
+print(random.randint(20,50))
+
+# Check if integer
+x = 7.0
+print(x.is_integer())
+y = 7.1 
+print(y.is_integer())
+
+z = 70.4
+print(isinstance(z, int))
