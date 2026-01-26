54c57ed68d3e8ccafed1d191b3368a35eb96a437
msattdev <mattsattdev@gmail.com>
2026-01-25 19:01:44 -0500

    advanced init

--- 8-NumericMastery/rounding.py
+++ 8-NumericMastery/rounding.py
@@ -9,3 +9,5 @@ print(math.floor(prize))
 print(math.ceil(prize))
 print(round(prize, 2))
 print(round(prize, 1))
+print(math.trunc(prize))  # Removes decimal part without rounding
+print(type(int(prize)))  # Converts to int, similar to trunc
