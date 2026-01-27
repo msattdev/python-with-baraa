00a6059147a24675abdfc2f540442feb64223082
msattdev <mattsattdev@gmail.com>
2026-01-27 09:31:19 -0500

    comparisons init

---
+++ 10-Comparisons/compoperators.py
@@ -0,0 +1,15 @@
+print('a' < 'b')    # True, because 'a' comes before 'b' in alphabetical order
+print(5 <= 10)        # True, because 5 is less than 10
+print(3 == 3)        # True, because both sides are equal
+print(4 != 5)        # True, because 4 is not equal to 5
+print(7 > 2)         # True, because 7 is greater than 2
+print(8 >= 8)        # True, because 8 is equal to 8
+print('cat' > 'car')  # True, because 't' comes after 'r' in alphabetical order
+print(15 < 10)       # False, because 15 is not less than 10
+print(6 == 7)        # False, because 6 is not equal to 7
+print(9 != 9)       # False, because 9 is equal to 9
+print(2 >= 3)       # False, because 2 is not greater than or equal to 3
+print('a' == 'b')    # False, because 'a' is not equal to 'b'
+print(1 < 4 < 6)   # True, because 1 is less than 4 and 4 is less than 6
+print(5 > 3 > 1)   # True, because 5 is greater than 3 and 3 is greater than 1
+print(2 < 3 > 4)   # False, because 3 is not greater than 4
