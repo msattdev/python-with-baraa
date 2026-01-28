ea9c9b1657d8a69c436286c599fbefbbca985b08
msattdev <mattsattdev@gmail.com>
2026-01-28 18:17:54 -0500

    identity init

---
+++ 12-IsIn/identity.py
@@ -0,0 +1,12 @@
+a = [1,2,3]
+print(a)
+b = [1,2,3]
+print(b)
+print(a is b)  # False, because a and b reference different objects in memory
+print(a == b)  # True, because a and b have the same content
+
+
+x = ['a', 'b', 'c']
+y = ['a', 'b', 'c']
+print(x == y)  # True, because x and y have the same content
+print(x is y)  # False, because x and y reference different objects in memory
