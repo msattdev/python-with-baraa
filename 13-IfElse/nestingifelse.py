51885c940790f86d76e470ead3c0c24c272c3bfa
msattdev <mattsattdev@gmail.com>
2026-01-29 10:13:29 -0500

    nested ifelse init

---
+++ 13-IfElse/nestingifelse.py
@@ -0,0 +1,17 @@
+# Nesting if-else statements
+score = 90
+submitted_project = True
+
+if score >= 90:
+    if submitted_project:
+        print("A+")
+    else:
+        print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
