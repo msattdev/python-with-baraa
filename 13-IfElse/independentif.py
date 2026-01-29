965a02e1b76af7f71a67b077bd8421b141206caf
msattdev <mattsattdev@gmail.com>
2026-01-29 10:23:23 -0500

    idependent if statement init

---
+++ 13-IfElse/independentif.py
@@ -0,0 +1,13 @@
+# Independent if example
+score = 90
+submitted_project = False
+
+if score >= 90:
+    print("High Score")
+else:
+    print("Low Score")
+
+if submitted_project:
+    print("Project Submitted")
+else:
+    print("Project Not Submitted")
