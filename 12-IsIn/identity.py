04d05195f4c27b198d7b51f292e24c6dc168cc42
msattdev <mattsattdev@gmail.com>
2026-01-29 09:48:06 -0500

    identity finale

--- 12-IsIn/identity.py
+++ 12-IsIn/identity.py
@@ -10,3 +10,6 @@ x = ['a', 'b', 'c']
 y = ['a', 'b', 'c']
 print(x == y)  # True, because x and y have the same content
 print(x is y)  # False, because x and y reference different objects in memory
+
+email = None
+print(email is not None and email != "") # True if email is not None and not an empty string
