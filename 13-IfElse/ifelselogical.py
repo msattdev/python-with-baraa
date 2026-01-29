fed98cc19eef01a63cf2705fda80ee94abb96826
msattdev <mattsattdev@gmail.com>
2026-01-29 10:19:26 -0500

    if else with logical operators

---
+++ 13-IfElse/ifelselogical.py
@@ -0,0 +1,16 @@
+# Using if else with logical operators
+score = 100
+submitted_project = False
+
+if score >= 90 and submitted_project:
+    print("A+")
+elif score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
