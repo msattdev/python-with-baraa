f61aa683add1bb2236b4e2cd6ecec5c18f894c29
msattdev <mattsattdev@gmail.com>
2026-01-30 17:45:28 -0500

    more updates

--- 15-ForLoops/forloops.py
+++ 15-ForLoops/forloops.py
@@ -2,3 +2,13 @@
 for i in (1,2,3,4,5):
     print(f'Round: {i}')
 
+items = [1,2,3,4,5, 'hi']
+for item in items:
+    print(f'Item: {item}')
+
+name = "Baraa"
+for letter in name:
+    print(f'Letter: {letter}')
+
+for num in range(1,5):
+    print(f'Number: {num}')
