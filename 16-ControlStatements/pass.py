5945e03c1f1f2ee4b9c94b67348bc6e32f986f12
msattdev <mattsattdev@gmail.com>
2026-02-01 10:16:14 -0500

    pass update

--- 16-ControlStatements/pass.py
+++ 16-ControlStatements/pass.py
@@ -2,6 +2,6 @@
 names = ["Alice", "Bob", "", "David"]
 for name in names:
     if name == "":
-        print("Empty value detected, skipping...")
+        # print("Empty value detected, skipping...")
         pass # Todo: handle empty names properly later
     print(f"Name: {name}") 
