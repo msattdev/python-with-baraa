d5d3f6d4c808c22fbec4576d45c93e06bcd7193d
msattdev <mattsattdev@gmail.com>
2026-02-01 10:15:48 -0500

    pass init

---
+++ 16-ControlStatements/pass.py
@@ -0,0 +1,7 @@
+# pass statement
+names = ["Alice", "Bob", "", "David"]
+for name in names:
+    if name == "":
+        print("Empty value detected, skipping...")
+        pass # Todo: handle empty names properly later
+    print(f"Name: {name}") 
