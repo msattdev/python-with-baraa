c7ee0c7a2774c8f44dc5ee00feb4bdf41e40f821
msattdev <mattsattdev@gmail.com>
2026-01-31 21:03:06 -0500

    control statements init

--- 15-ForLoops/forchallenge1.py
+++ 15-ForLoops/forchallenge1.py
@@ -1,3 +1,9 @@
 # Print the 7-times table from 1 to 10 using a for loop
 for i in range(1,11):
     print(f"7 x {i} = {7 * i}")
+
+# Calculate the sum of all even numbers between 1 and 50
+even_sum = 0
+for num in range(2, 51, 2):
+    even_sum += num
+print(f"Sum of even numbers between 1 and 50: {even_sum}")
