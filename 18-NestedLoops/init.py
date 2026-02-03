ca65eb4badb0fed664e4592a953897cdfd4999da
msattdev <mattsattdev@gmail.com>
2026-02-03 09:29:44 -0500

    added nested loops init

--- 18-NestedLoops/init.py
+++ 18-NestedLoops/init.py
@@ -1,3 +1,4 @@
 for x in range(3): # outer loop
     for y in range(2): # inner loop
-        print(f'Outer loop iteration: {x}, Inner loop iteration: {y}')
+        for z in range(2): # innermost loop
+            print(f'Outer loop iteration: {x}, Inner loop iteration: {y}, Innermost loop iteration: {z}')
