bbb05e90340ac4cc22f5960cd5f68dcc0f5a944a
msattdev <mattsattdev@gmail.com>
2026-02-04 09:41:31 -0500

    more updates

---
+++ 19-WhileLoops/whiletask.py
@@ -0,0 +1,7 @@
+# Write a program that keeps asking:
+# "Do you agree?"
+# until the user types "yes"
+user_answer = ""
+while user_answer != "yes":
+    user_answer = input("Do you agree?(yes/no): ")
+print("Thank you")
