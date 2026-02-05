5053f18da4e4b1dce4d85e8b79345a361a9fc013
msattdev <mattsattdev@gmail.com>
2026-02-04 20:31:40 -0500

    lists update

--- 20-Lists/lists.py
+++ 20-Lists/lists.py
@@ -6,3 +6,8 @@ print(empty)
 print(type(empty))
 print(letters)
 print(type(letters))
+
+my_string = "Hello"
+print(my_string)
+string_list = list(my_string) # convert string to list
+print(string_list)
