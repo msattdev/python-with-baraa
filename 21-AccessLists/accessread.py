26d598add94d0ac64f09668bf1ad19b19ef8fa8f
msattdev <mattsattdev@gmail.com>
2026-02-04 20:41:53 -0500

    access and read init

---
+++ 21-AccessLists/accessread.py
@@ -0,0 +1,7 @@
+# Access & Read Lists
+lst = ['a','b','c','d','e']
+print(lst)
+print(lst[0])  # First element
+print(lst[2])  # Third element
+print(lst[-1]) # Last element
+print(lst[-2]) # Second last element
