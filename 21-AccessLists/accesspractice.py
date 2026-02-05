64e348898a966a8301b518bcb87db1a19ffde9ac
msattdev <mattsattdev@gmail.com>
2026-02-05 11:04:00 -0500

    access and read updates

---
+++ 21-AccessLists/accesspractice.py
@@ -0,0 +1,11 @@
+# Access and Read
+matrix = [
+    ['a','b','c'],
+    ['d','e','f'],
+    ['g','h','i']
+] # 3x3 matrix of letters
+
+# print(matrix)
+print(matrix[-1])       # Last row
+print(matrix[-1][-1])   # Last element of the last row
+print(matrix[0][1:3])   # Slice of first row from index 1 to 3)
