bfedf8f4e217cd4fb4a506a0b94c8af37782198c
msattdev <mattsattdev@gmail.com>
2026-02-06 17:16:25 -0500

    list counting

---
+++ 23-ExploreAnalyze/count.py
@@ -0,0 +1,6 @@
+# Count with lists
+my_list = ['a','b','b','c','c','c','c','d','e','f']
+
+# Count occurrences of 'c'
+count_c = my_list.count('c')
+print(f"The letter 'c' appears {count_c} times in the list.")
