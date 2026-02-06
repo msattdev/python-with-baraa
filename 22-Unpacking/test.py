0398356e0b6d44163544e2af5675b6497e27ae72
msattdev <mattsattdev@gmail.com>
2026-02-06 10:14:56 -0500

    list unpacking init

---
+++ 22-Unpacking/test.py
@@ -0,0 +1,10 @@
+# Unpacking. Asterisk and underscore
+person = ['Maria',29,'Data Engineer','Spain']
+# name = person[0]
+# age = person[1]
+# role = person[2]
+# country = person[3]
+# print(name, age, role, country)
+
+name, age, role, country = person
+print(name, age, role, country)
