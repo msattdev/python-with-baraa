8f75a83d423f39fe6ff33dd78eb23270dcddd4a2
msattdev <mattsattdev@gmail.com>
2026-02-06 17:02:31 -0500

    list functions updates

--- 23-ExploreAnalyze/test.py
+++ 23-ExploreAnalyze/test.py
@@ -4,3 +4,8 @@ print("Max:", max(numbers))
 print("Min:", min(numbers))
 print("Sum:", sum(numbers))
 print("Length:", len(numbers))
+
+print("All:", all(numbers))
+print("All:", all([1,0,2]))
+print("Any:", any([1,0,2]))
+print("Any:", any([0,0,0]))
