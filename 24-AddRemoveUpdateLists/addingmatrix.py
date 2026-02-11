6135494cec8916261348b30f53784f28287b31b0
msattdev <mattsattdev@gmail.com>
2026-02-11 17:57:31 -0500

    updates

--- 24-AddRemoveUpdateLists/addingmatrix.py
+++ 24-AddRemoveUpdateLists/addingmatrix.py
@@ -6,8 +6,11 @@ matrix = [
     ['g', 'h', 'i']  # row 2
 ]
 
-matrix.append(['x', 'y', 'z']) # add a new row to the end of the matrix
-matrix.insert(1, ['a','a','a']) # add a new row at index 1
-matrix[0].append('d') # add a new item to the end of row 0
-matrix[2].insert(1, 'x') # add a new item at index 1 of row 2
+# matrix.append(['x', 'y', 'z']) # add a new row to the end of the matrix
+# matrix.insert(1, ['a','a','a']) # add a new row at index 1
+# matrix[0].append('d') # add a new item to the end of row 0
+# matrix[2].insert(1, 'x') # add a new item at index 1 of row 2
+
+matrix[1].remove('e') # remove the first occurrence of 'e' from row 1
+matrix[-1].pop(0) # remove the item at index 0 of the last row (which is 'g')
 print(matrix)
--- 24-AddRemoveUpdateLists/removefromlist.py
+++ 24-AddRemoveUpdateLists/removefromlist.py
@@ -8,3 +8,7 @@ print(letters)
 
 letters.pop(1) # remove the item at index 1 (which is 'b') from the list
 print(letters)
+
+removed = letters.pop(2) # remove the item at index 2 (which is 'e') from the list and store it in a variable
+print('Removed item:', removed)
+print(letters)
