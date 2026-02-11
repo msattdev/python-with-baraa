0d068582478068ba30124182f1f0698fec35d37d
msattdev <mattsattdev@gmail.com>
2026-02-11 17:39:23 -0500

    adding items to lists init

--- 21-AccessLists/accesspractice.py
+++ 21-AccessLists/accesspractice.py
@@ -8,4 +8,4 @@ matrix = [
 # print(matrix)
 print(matrix[-1])       # Last row
 print(matrix[-1][-1])   # Last element of the last row
-print(matrix[0][1:3])   # Slice of first row from index 1 to 3)
+print(matrix[0][1:3])   # Slice of first row from index 1 to 3
---
+++ 24-AddRemoveUpdateLists/additems.py
@@ -0,0 +1,6 @@
+letters = ['a,','b', 'c']
+
+letters.append('x')
+letters.append('y')
+letters.append('z')
+print(letters)
