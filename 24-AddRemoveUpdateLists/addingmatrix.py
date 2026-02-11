2ebb2984bc3bdb2fce6294a4e85674cd7033313b
msattdev <mattsattdev@gmail.com>
2026-02-11 18:03:23 -0500

    updates

--- 24-AddRemoveUpdateLists/addingmatrix.py
+++ 24-AddRemoveUpdateLists/addingmatrix.py
@@ -11,6 +11,10 @@ matrix = [
 # matrix[0].append('d') # add a new item to the end of row 0
 # matrix[2].insert(1, 'x') # add a new item at index 1 of row 2
 
-matrix[1].remove('e') # remove the first occurrence of 'e' from row 1
-matrix[-1].pop(0) # remove the item at index 0 of the last row (which is 'g')
+# matrix[1].remove('e') # remove the first occurrence of 'e' from row 1
+# matrix[-1].pop(0) # remove the item at index 0 of the last row (which is 'g')
+
+matrix[-1] = ['x', 'y', 'z'] # replace the last row with a new row
+matrix[0][0] = 'x' # replace the item at index 0 of row 0 with 'x'
+matrix[1][1] = 'y' # replace the item at index 1 of row 1 with 'y'
 print(matrix)
---
+++ 24-AddRemoveUpdateLists/updateitems.py
@@ -0,0 +1,7 @@
+# Update items in a list
+letters = ['a', 'b', 'c', 'd', 'e']
+
+letters[0] = 'x' # update the item at index 0 to 'x'
+letters[2] = 'y' # update the item at index 2 to 'y'
+letters[-1] = 'z' # update the last item to 'z'
+print(letters)
