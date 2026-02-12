dd8da528ece185860f8ba2a2c61a5c7d79014afa
msattdev <mattsattdev@gmail.com>
2026-02-12 09:03:41 -0500

    order lists updates

--- 25-OrderLists/order.py
+++ 25-OrderLists/order.py
@@ -1,9 +1,14 @@
 # Sort lists
 letters = ['d', 'b', 'a', 'e', 'c']
-letters.sort() # Sort the list in ascending order
-print(letters)
-letters.sort(reverse=True) # Sort the list in descending order
-print(letters)
+# letters.sort() # Sort the list in ascending order
+# print(letters)
+# letters.sort(reverse=True) # Sort the list in descending order
+# print(letters)
+new_list = sorted(letters) # Create a new sorted list without modifying the original
+reverse_list = sorted(letters, reverse=True) # Create a new sorted list in descending order
+print('Original List:', letters)
+print('Reversed List:',reverse_list)
+print('Sorted List:',new_list)
 
 matrix = [
     [3, 1, 2],
