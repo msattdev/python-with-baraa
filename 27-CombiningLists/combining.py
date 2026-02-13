470464274dcff41dc9334da152f214bf656f83fc
msattdev <mattsattdev@gmail.com>
2026-02-13 10:18:56 -0500

    combining updates

--- 27-CombiningLists/combining.py
+++ 27-CombiningLists/combining.py
@@ -2,5 +2,17 @@
 letters = ['a','b','c']
 numbers = [1,2,3]
 # Using the + operator
-combined = letters + numbers
-print(combined) # ['a', 'b', 'c', 1, 2, 3]
+# combined = letters + numbers
+# print(combined) # ['a', 'b', 'c', 1, 2, 3]
+
+# Using the nested list option
+combined = [letters, numbers]
+print(combined) # [['a', 'b', 'c'], [1, 2, 3]]
+
+# Using the * operator
+print(letters * 2) # ['a', 'b', 'c', 'a', 'b', 'c']
+
+# Using the extend() method
+numbers.extend(letters)
+print(letters) # ['a', 'b', 'c']
+print(numbers) # [1, 2, 3, 'a', 'b', 'c']
